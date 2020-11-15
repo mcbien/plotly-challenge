586c64d25ae470a0eca362264bdd75555769e7ac
mcbien <mcbien1122@gmail.com>
2020-11-14 19:01:08 -0600

    Saving homework progress

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,34 @@
+// Use d3.json to read local json file
+//      Set path to data file
+var filepath = "../samples.json"
+
+function importData() {
+
+    d3.json(filepath).then(function (data) {
+        // console.log(data);
+        // console.log("Keys");
+        // console.log(Object.keys(data));
+        // console.log("Values");
+        // console.log(Object.values(data));
+        // console.log("Entries");
+        // console.log(Object.entries(data));
+
+        // Break data object into component parts
+        // var names = data.names;
+        // console.log("Names");
+        // console.log(names);
+        // var metadata = data.metadata;
+        // console.log("Metadata");
+        // console.log(metadata)
+        // console.log("Samples");
+        // var samples = data.samples;
+        // console.log(samples)
+
+        // Use map to enumerate the values within metadata
+        var Id = data.metadata.map(item => item.id);
+        console.log(Id)
+    })
+
+}
+
+importData()
