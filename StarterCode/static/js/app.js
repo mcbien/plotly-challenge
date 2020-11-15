6bff8d6773ee35d8f86a17743cc41bd248c6fdac
mcbien <mcbien1122@gmail.com>
2020-11-14 19:33:28 -0600

    Saving homework progress

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,24 +5,33 @@ var filepath = "../samples.json"
 function importData() {
 
     d3.json(filepath).then(function (data) {
-        // console.log(data);
-        // console.log("Keys");
-        // console.log(Object.keys(data));
-        // console.log("Values");
-        // console.log(Object.values(data));
-        // console.log("Entries");
-        // console.log(Object.entries(data));
+        console.log(data);
+        console.log("Keys");
+        console.log(Object.keys(data));
+        console.log("Values");
+        console.log(Object.values(data));
+        console.log("Entries");
+        console.log(Object.entries(data));
 
-        // Break data object into component parts
-        // var names = data.names;
-        // console.log("Names");
-        // console.log(names);
-        // var metadata = data.metadata;
-        // console.log("Metadata");
-        // console.log(metadata)
-        // console.log("Samples");
-        // var samples = data.samples;
-        // console.log(samples)
+        //Break data object into component parts
+        var names = data.names;
+        console.log("Names");
+        console.log(names);
+        var metadata = data.metadata;
+        console.log("Metadata");
+        console.log(metadata)
+        console.log("Samples");
+        var samples = data.samples;
+        console.log(samples);
+
+        //find the keys
+        console.log("Sample Keys");
+        console.log(Object.keys(samples));
+
+
+
+
+
 
         // Use map to enumerate the values within metadata
         var Id = data.metadata.map(item => item.id);
@@ -31,4 +40,6 @@ function importData() {
 
 }
 
+
+// Call function
 importData()
