58bd77ed623197c883c6ff0440b53d06a95e6cda
mcbien <mcbien1122@gmail.com>
2020-11-15 14:26:06 -0600

    Reviewed and mapped json file and desired output from screenshots

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,49 @@
 // Use d3.json to read local json file
 //      Set path to data file
+
+// ***** Review of samples.json: key and structure
+//          names = simple array
+//          metadata = array of objects
+//              object keys: id, ethnicity, gender, age, location, bbtype, wfreq
+//          samples = array of objects contaning arrays
+//              object keys: id. otu_ids, sample_values, otu_labels
+
+
+// Review of screenshots:
+//  dashboard_part1.png & dashboard_part 2:
+//     1. Select Sample: dropdown using ID: 940 >> metadata: id
+//     2. Sample MetaData: Unordered list with:
+//          Age: 24 >> metadata: age
+//          BBTYPE: I >> metadata: bbtype
+//          ETHNICITY: Caucasian >> metadata: ethnicity
+//          GENDER: F >> metadata: gender
+//          LOCATION: Beaufort/NC >> mwetadata: location
+//          WFREQ: 2 >> metadata: wfreq
+//          sample: 940 >> metadata: id
+//     3. pie chart: % by samples: otu_id, samples: otu_id as label
+//     4. bubble chart:
+//           x-axis: >> samples: otu_id
+//           y-axis: >> samples: sample_values
+//           marker size >> samples: sample_values
+//           marker color >> samples: otu_id
+//           bubble label: >> samples: otu_labels
+//     5. Belly Button Washing Frequency speedometer
+//          Scrubs per week: >> metadata: wfreq
+//  hw01.png: Horizontal bar
+//          y-axis: samples: otu_ids
+//          x-axis: samples: sample_values
+//    
+
+
+
+
+
 var filepath = "../samples.json"
+
+
+
+
+
 
 function importData() {
 
